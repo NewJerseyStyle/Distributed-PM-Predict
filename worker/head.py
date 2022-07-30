cc8c0e23aecca7e4272707569f5a0ef99632fe70
Stupid <22520563+NewJerseyStyle@users.noreply.github.com>
2022-07-31 00:33:16 +0930

    implementing worker with ray

--- worker/head.py
+++ worker/head.py
@@ -5,6 +5,10 @@ from pyppeteer import launch
 from pyppeteer_stealth import stealth
 from tinydb import TinyDB, Query
 from tqdm import tqdm
+import ray
+
+ray.init()
+
 
 async def download_all_mps():
     print('[download_all_mps] Start')
@@ -108,12 +112,9 @@ def get_all_powers():
     return power_list
 
 
-async def ask_google(pname):
-    print('[ask_google] Start')
-    with TinyDB('db.json') as db:
-        pc_db = db.table('pcs')
-        at_db = db.table('articles')
-        User = Query()
+@ray.remote
+def ask_google(pname, pc_name, engine):
+    async def ask(pname, pc_name, engine):
         browser = await launch(headless=True,
                                args=["--disable-gpu",
                                      "--no-sandbox",
@@ -121,39 +122,72 @@ async def ask_google(pname):
                                      "--disable-dev-shm-usage",
                                      "--no-first-run",
                                      "--single-process"])
+        at_db_list = []
+        page = await browser.newPage()
+        await stealth(page)
+        # google pname + pc.name
+        await page.goto('https://www.google.com/')
+        await page.waitForSelector('input')
+        await page.type('input', f'{pname} and {pc_name}')
+        await page.keyboard.press('Enter')
+        # grap all result of first 2 page
+        for i in range(2):
+            try:
+                await page.waitForSelector('.g')
+                article_list = await page.querySelectorAll('.g')
+                for element in article_list:
+                    article = await page.evaluate('(element) => element.classList.length > 1 ? "" : element.innerText', element)
+                    at_db_list.append(article)
+                element = await page.querySelector('a#pnnext')
+                if element is None:
+                    tqdm.write(f'[ask_google] {pname} + {pc_name} no next page, break')
+                    break
+                await page.evaluate('(element) => element.click()', element)
+            except Exception as e:
+                tqdm.write(f'[ask_google] Google page {i} with {pc_name} got {repr(e)}')
+            finally:
+                await asyncio.sleep(3)
+        await page.close()
+        await asyncio.sleep(random.randint(14, 30))
+        data = []
+        for article in at['texts']:
+            if at['pc'] in article or len(
+                [n for n in pc_name if n in at_db_list]):
+                data.append(article)
+        if engine == Engine.NLTK:
+            supportiveness, _, _ = nltk_sent.sentiment_analysis(data)
+        elif engine == Engine.HUGGINGFACE:
+            supportiveness, _, _ = deep_sent.sentiment_analysis(data)
+        await browser.close()
+        return supportiveness, len(data), len(at_db_list)
+    return asyncio.get_event_loop().run_until_complete(
+        asyncio.gather(
+            ask(pname, pc_name, engine)))
+
+
+def ask_google_tasks(pname, engine=Engine.NLTK):
+    assert isinstance(engine, Engine)
+
+    print('[ask_google] Start')
+    with TinyDB('db.json') as db:
+        pc_db = db.table('pcs')
+        at_db = db.table('articles')
+        User = Query()
+        tasks = []
         for pc in tqdm(pc_db):
             pc_name = pc['name']
-#             tqdm.write(f'[ask_google] Googling with {pc_name}')
-            at_db_list = []
-            page = await browser.newPage()
-            await stealth(page)
-            # google pname + pc.name
-            await page.goto('https://www.google.com/')
-            await page.waitForSelector('input')
-            await page.type('input', f'{pname} and {pc_name}')
-            await page.keyboard.press('Enter')
-            # grap all result of first 2 page
-            for i in range(2):
-                try:
-                    await page.waitForSelector('.g')
-                    article_list = await page.querySelectorAll('.g')
-                    for element in article_list:
-                        article = await page.evaluate('(element) => element.classList.length > 1 ? "" : element.innerText', element)
-                        at_db_list.append(article)
-                    element = await page.querySelector('a#pnnext')
-                    if element is None:
-                        tqdm.write(f'[ask_google] {pname} + {pc_name} no next page, break')
-                        break
-                    await page.evaluate('(element) => element.click()', element)
-                except Exception as e:
-                    tqdm.write(f'[ask_google] Google page {i} with {pc_name} got {repr(e)}')
-                finally:
-                    await asyncio.sleep(3)
-            await page.close()
-            await asyncio.sleep(random.randint(30, 40))
-        # store data to db
-        at_db.upsert({'name': pname, 'pc': pc_name, 'texts': at_db_list}, (User.name == pname) & (User.pc == pc_name))
-        await browser.close()
+            task = ask_google.remote(pname, pc_name, engine)
+            tasks.append(task)
+        return_vals = ray.get(tasks)
+        activeness_denominator = max([x for _, _, x in return_vals])
+        ds_db = db.table('qips')
+        for supportiveness, activeness_numerator, _ in return_vals:
+            ds = {}
+            activeness = 100 * activeness_numerator / activeness_denominator
+            powerfulness = 50
+            if pname in get_all_powers():
+                powerfulness = 70
+            # so whoh p-pc pair is this?? more work to be done
 
 
 def do_search():
@@ -161,9 +195,9 @@ def do_search():
     db = TinyDB('db.json')
     mp_db = db.table('mps')
     for mp in tqdm(mp_db, desc='Googleing supporting MPs'):
-        asyncio.get_event_loop().run_until_complete(ask_google(mp['name']))
+        asyncio.get_event_loop().run_until_complete(ask_google_tasks(mp['name']))
     for name in tqdm(get_all_powers(), desc='Googleing supporting powers'):
-        asyncio.get_event_loop().run_until_complete(ask_google(name))
+        asyncio.get_event_loop().run_until_complete(ask_google_tasks(name))
     print('[do_search] End...')
 
 
